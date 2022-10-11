39939a745d9f163cf7a647bc4243fbb083b3cc83
Kamrul Islam Tushar <43824192+kamrulislamtushar@users.noreply.github.com>
2022-10-11 16:15:46 +0600

    Time: 31 ms (19.47%), Space: 68.8 MB (57.90%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+     public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(nums[i])) {
+                return true;
+            }
+            hashMap.put(nums[i], i);
+        }
+        return false;
+    }
+}
